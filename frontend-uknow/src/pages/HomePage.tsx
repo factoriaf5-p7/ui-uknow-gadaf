73fc2c6d5946f8eefbc31025060333d4284b6ae0
AleNS <isalejanaranjo@gmail.com>
2023-07-25 20:59:51 +0200

    Desktop navbar created, missing to test functionality when scrolls

--- frontend-uknow/src/pages/HomePage.tsx
+++ frontend-uknow/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { Categories } from '../components/Categories/Categories'
 import { Header } from '../components/Header/Header'
 import {NavbarBottom} from '../components/NavbarBottom/NavbarBottom'
+import { NavbarDesk } from '../components/NavbarDesk/NavbarDesk'
 import { PopularCourses } from '../components/PopularCourses/PopularCourses'
 
 export function HomePage () {
   return (
     <>
+      <NavbarDesk/>
       <Header/>
       <Categories />
       <PopularCourses />
