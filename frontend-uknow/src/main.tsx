4417c439307563165a3327d6702e0e8424c2dbaa
Georgina <georginatomass@gmail.com>
2023-07-25 13:35:20 +0200

    antes push dev

--- frontend-uknow/src/main.tsx
+++ frontend-uknow/src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./components/routes";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './components/routes'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
