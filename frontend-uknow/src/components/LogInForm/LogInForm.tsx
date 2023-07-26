8ff4399f518b3aac28574a9c6078c8f66ca621a2
AleNS <isalejanaranjo@gmail.com>
2023-07-26 20:00:19 +0200

    Calling the api for the loging process

--- frontend-uknow/src/components/LogInForm/LogInForm.tsx
+++ frontend-uknow/src/components/LogInForm/LogInForm.tsx
@@ -3,14 +3,23 @@ import { ButtonP } from "../../components/ButtonP";
 import { Form } from "react-bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import styles from "./LogInForm.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function LogInForm() {
+  const navigate = useNavigate()
+  const initialState ={email:'',password:''};
   const [formData, setFormData] = useState({ email: "", password: "" });
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Form submitted!"); // Add a console log to check if the function is called
-    console.log(formData);
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log("Form submitted!")
+    console.log(formData)
+    const response = await axios.post('http://localhost:3000/auth/login',formData, {headers:{"Content-Type": "application/JSON"}} )
+    console.log(response.data)
+    localStorage.setItem('token',response.data.data);
+    navigate('/profile')
+    setFormData(initialState);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
