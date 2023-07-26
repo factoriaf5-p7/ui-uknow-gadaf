5183c0a5ea0a5999c07f75b51c46aa4e8601c08b
Federico Javier Martino <federicojaviermartino@gmail.com>
2023-07-26 12:37:21 +0200

    interface

--- frontend-uknow/src/components/Interfaces/CourseInterface.tsx
+++ frontend-uknow/src/components/Interfaces/CourseInterface.tsx
@@ -6,18 +6,3 @@ export interface Course {
     tags: string;
     content: string;
 }
-
-
-//   {
-//     _id: string;
-//     image_url: string;
-//     name: string;
-//     tagline: string;
-//     first_brewed: string;
-//     attenuation_level: number;
-//     description: string;
-//     contributed_by: string;
-//     _v: string;
-//     brewers_tips: string;
-//     expireAt: string;
-// }
