93b18d64e538fa2c62dec0d10ef7178534d79a0b
Georgina <georginatomass@gmail.com>
2023-07-26 15:49:11 +0200

    resolver errores eslint + app.css + primer testhomepage

--- frontend-uknow/src/pages/LandingPage/LandingPage.tsx
+++ frontend-uknow/src/pages/LandingPage/LandingPage.tsx
@@ -1,35 +1,31 @@
-import { Link } from "react-router-dom"
-import { ButtonP } from "../../components/ButtonP"
-import { ButtonS } from "../../components/ButtonS"
-import homework from "../../assets/homework.mp4"
-import  { useEffect, useState } from "react";
-import styles from './LandingPage.module.css';
-import Logo from "../../assets/LOGO.png"
+import { Link } from 'react-router-dom'
+import { ButtonP } from '../../components/ButtonP'
+import { ButtonS } from '../../components/ButtonS'
+import homework from '../../assets/homework.mp4'
+import { useEffect, useState } from 'react'
+import styles from './LandingPage.module.css'
+import Logo from '../../assets/LOGO.png'
 
-
-
-export const LandingPage=()=> {
-
-  const [videoEnded, setVideoEnded] = useState(false);
-  const [showLogo, setShowLogo] = useState(false);
+export const LandingPage = () => {
+  const [videoEnded, setVideoEnded] = useState(false)
+  const [showLogo, setShowLogo] = useState(false)
 
   const handleVideoEnded = () => {
-    setVideoEnded(true);
-  };
+    setVideoEnded(true)
+  }
 
   useEffect(() => {
     // Delay showing the logo for 500ms after the video ends
     if (videoEnded) {
       setTimeout(() => {
-        setShowLogo(true);
-      }, 150);
+        setShowLogo(true)
+      }, 150)
     }
-  }, [videoEnded]);
-
+  }, [videoEnded])
 
   return (
     <section className={styles.hero}>
-       <video
+      <video
         className={styles.video}
         src={homework}
         autoPlay
@@ -38,22 +34,21 @@ export const LandingPage=()=> {
       >
         Your browser does not support the video tag.
       </video>
-      
+
       <div className={`${styles['logo-container']} ${showLogo ? styles['show-logo'] : ''}`}>
-        <img className={styles.logo} src={Logo} alt="U-know logo" />
+        <img className={styles.logo} src={Logo} alt='U-know logo' />
       </div>
-      
-      <div className={`${styles["buttons-container"]} ${videoEnded ? styles["translate-up"] : ""}`}>
-        
-      
+
+      <div className={`${styles['buttons-container']} ${videoEnded ? styles['translate-up'] : ''}`}>
+
         <p>Sign up and get 1000 knowlitos to get you started</p>
-          <Link   to="/loginsignup">
-            <ButtonP className={styles.button} text={"Get Started"} />
-          </Link>
-          <Link  to="/home">
-            <ButtonS className={styles.button} text={"Explore"} />
-          </Link>
-      </div>  
+        <Link to='/loginsignup'>
+          <ButtonP className={styles.button} text='Get Started' />
+        </Link>
+        <Link to='/home'>
+          <ButtonS className={styles.button} text='Explore' />
+        </Link>
+      </div>
     </section>
   )
 }
--- frontend-uknow/src/pages/LogInSignUp/LogInSignUp.tsx
+++ frontend-uknow/src/pages/LogInSignUp/LogInSignUp.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import { Form } from "react-bootstrap";
-import { ButtonP } from "../../components/ButtonP";
-import styles from "./LogInSignUp.module.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import Logo from "../../assets/LOGO.png";
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
+import { ButtonP } from '../../components/ButtonP'
+import styles from './LogInSignUp.module.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Logo from '../../assets/LOGO.png'
 
 export const LogInSignUp = () => {
-  const [activeTab, setActiveTab] = useState("logIn");
+  const [activeTab, setActiveTab] = useState('logIn')
 
   return (
     <div className={styles.content}>
       <div className={styles.header}>
         <h2 id={styles.welcome}>Welcome to</h2>
         <div className={styles.logo}>
-          <img src={Logo} alt="U-know logo" />
+          <img src={Logo} alt='U-know logo' />
         </div>
         <p>Please enter your details</p>
       </div>
@@ -22,84 +22,86 @@ export const LogInSignUp = () => {
         <div className={styles.tabs}>
           <div
             className={`${styles.tab} ${
-              activeTab === "logIn" ? styles.active : ""
+              activeTab === 'logIn' ? styles.active : ''
             }`}
-            onClick={() => setActiveTab("logIn")}
+            onClick={() => setActiveTab('logIn')}
           >
             <p>Log In</p>
           </div>
           <div
             className={`${styles.tab} ${
-              activeTab === "signUp" ? styles.active : ""
+              activeTab === 'signUp' ? styles.active : ''
             }`}
-            onClick={() => setActiveTab("signUp")}
+            onClick={() => setActiveTab('signUp')}
           >
             <p>Sign Up</p>
           </div>
         </div>
 
-        {activeTab === "logIn" ? (
-          <div className={styles.logInContainer}>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter your email" />
-              </Form.Group>
+        {activeTab === 'logIn'
+          ? (
+            <div className={styles.logInContainer}>
+              <Form>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Control type='email' placeholder='Enter your email' />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control
-                  id={styles.textField}
-                  type="password"
-                  placeholder="Enter your password"
-                />
-              </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                  <Form.Control
+                    id={styles.textField}
+                    type='password'
+                    placeholder='Enter your password'
+                  />
+                </Form.Group>
 
-              <Form.Text id={styles.forgotPassword}>Forgot password?</Form.Text>
+                <Form.Text id={styles.forgotPassword}>Forgot password?</Form.Text>
 
-              <ButtonP text={"Log In"} />
-            </Form>
-          </div>
-        ) : (
-          <div className={styles.signUpContainer}>
-            <Form>
-              <Form.Group className="mb-3">
-                <div className="input-icon">
-                  <Form.Control type="email" placeholder="Enter your name" />
-                </div>
-              </Form.Group>
+                <ButtonP text='Log In' />
+              </Form>
+            </div>
+            )
+          : (
+            <div className={styles.signUpContainer}>
+              <Form>
+                <Form.Group className='mb-3'>
+                  <div className='input-icon'>
+                    <Form.Control type='email' placeholder='Enter your name' />
+                  </div>
+                </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Control type="email" placeholder="Enter your email" />
-              </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Control type='email' placeholder='Enter your email' />
+                </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Control
-                  id={styles.textField}
-                  type="password"
-                  placeholder="Enter your password"
-                />
-              </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Control
+                    id={styles.textField}
+                    type='password'
+                    placeholder='Enter your password'
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Control
-                  id={styles.textField}
-                  type="password"
-                  placeholder="Confirm your password"
-                />
-              </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Control
+                    id={styles.textField}
+                    type='password'
+                    placeholder='Confirm your password'
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check
-                  type="checkbox"
-                  label="By signing you agree to our Terms of service and Privacy policy"
-                  style={{ fontSize: "12px", color: "#777777" }}
-                />
-              </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                  <Form.Check
+                    type='checkbox'
+                    label='By signing you agree to our Terms of service and Privacy policy'
+                    style={{ fontSize: '12px', color: '#777777' }}
+                  />
+                </Form.Group>
 
-              <ButtonP text={"Sign Up"} />
-            </Form>
-          </div>
-        )}
+                <ButtonP text='Sign Up' />
+              </Form>
+            </div>
+            )}
       </div>
     </div>
-  );
-};
+  )
+}
