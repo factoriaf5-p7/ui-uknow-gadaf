147d9985465830b0fc35966f26dad14cc38bd1fa
Georgina <georginatomass@gmail.com>
2023-07-27 09:56:31 +0200

    homePage test

--- frontend-uknow/src/pages/CoursePage.tsx
+++ frontend-uknow/src/pages/CoursePage.tsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Course } from "../components/Interfaces/CourseInterface";
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Course } from '../components/Interfaces/CourseInterface'
 
 export const CoursePage = () => {
-    const { CourseId } = useParams<{ CourseId: string }>()
-    const [course, setCourse] = useState<Course>()
+  const { CourseId } = useParams<{ CourseId: string }>()
+  const [course, setCourse] = useState<Course>()
 
-    useEffect(() => {
-        const fetchCourse = async () => {
-            try {
-                const response = await fetch(
+  useEffect(() => {
+    const fetchCourse = async () => {
+      try {
+        const response = await fetch(
                     `http://localhost:5173/HomePage/${CourseId}`
-                )
-                const data = await response.json()
-                setCourse(data)
-                console.log(data)
-            } catch (error) {
-                console.log('No se encuentra ese curso por su:', error)
-            }
-        }
-        fetchCourse()
-    }, [CourseId])
+        )
+        const data = await response.json()
+        setCourse(data)
+        console.log(data)
+      } catch (error) {
+        console.log('No se encuentra ese curso por su:', error)
+      }
+    }
+    fetchCourse()
+  }, [CourseId])
 
-    return (
-        <div>
-            {course && (
-                <>
-                    <h2>{course.name}</h2>
-                    <p>{course.price}</p>
-                    <p>{course.topic}</p>
-                    <p>Attenuation Level: {course.difficulty}</p>
-                    <p>{course.tags}</p>
-                    <p>{course.content}</p>
-                </>
-            )}
-            {!course && <p>Cargando Cursos...</p>}
-        </div>
-    )
+  return (
+    <div>
+      {course && (
+        <>
+          <h2>{course.name}</h2>
+          <p>{course.price}</p>
+          <p>{course.topic}</p>
+          <p>Attenuation Level: {course.difficulty}</p>
+          <p>{course.tags}</p>
+          <p>{course.content}</p>
+        </>
+      )}
+      {!course && <p>Cargando Cursos...</p>}
+    </div>
+  )
 }
--- frontend-uknow/src/test/homePage.test.tsx
+++ frontend-uknow/src/test/homePage.test.tsx
@@ -1,17 +1,17 @@
 import { describe, test, expect, beforeEach } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
-import { HomePage } from '../pages/HomePage'
+// import { MemoryRouter } from 'react-router-dom'
+// import { HomePage } from '../pages/HomePage'
 
 describe('HOMEPAGE', () => {
   describe('HomePage components', () => {
-    beforeEach(() => {
-      render(
-        <MemoryRouter initialEntries={['/']}>
-          <HomePage />
-        </MemoryRouter>
-      )
-    })
+    // beforeEach(() => {
+    //   render(
+    //     <MemoryRouter initialEntries={['/']}>
+    //       <HomePage />
+    //     </MemoryRouter>
+    //   )
+    // })
 
     test('Renders title Categories', async () => {
       await waitFor(() => {
