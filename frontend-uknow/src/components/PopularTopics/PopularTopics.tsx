b2478e2269cb25c6c613f7989babee83a542088e
andreubltzr11 <andreubltzr11@gmail.com>
2023-07-27 10:51:24 +0200

    minor changes

--- frontend-uknow/src/components/PopularTopics/PopularTopics.tsx
+++ frontend-uknow/src/components/PopularTopics/PopularTopics.tsx
@@ -1,35 +1,34 @@
-import { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
-import { TopicChip } from "./TopicChip";
+import { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+import { TopicChip } from './TopicChip'
 
 export const PopularTopics = () => {
-    const [topics, setTopics] = useState<any[]>([]);
-  
-    useEffect(() => {
-      const fetchTopics = async () => {
-        try {
-          const response = await fetch("http://localhost:3000/api/courses");
-          const data = await response.json();
-          setTopics(data.data);
-        } catch (error) {
-          console.log("No se encuentra ese tema por su:", error);
-        }
-      };
-      fetchTopics();
-    }, []);
-  
-    return (
-      <Container>
-        <h2>Popular topics</h2>
-        <div>
-          {topics.map((topic) => (
-            <TopicChip key={topic.id} title={topic.name} />
-          ))}
-        </div>
-      </Container>
-    );
-  };
-  
+  const [topics, setTopics] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/courses')
+        const data = await response.json()
+        setTopics(data.data)
+      } catch (error) {
+        console.log('No se encuentra ese tema por su:', error)
+      }
+    }
+    fetchTopics()
+  }, [])
+
+  return (
+    <Container>
+      <h2>Popular topics</h2>
+      <div>
+        {topics.map((topic) => (
+          <TopicChip key={topic.id} title={topic.name} />
+        ))}
+      </div>
+    </Container>
+  )
+}
 
 {
   /* <TopicChip title='CSS'/> */
