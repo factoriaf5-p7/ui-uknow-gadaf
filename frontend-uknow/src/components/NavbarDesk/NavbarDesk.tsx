f49a0341de8ae4e52ba0c877ea211ce374fe2616
Teresa <teresamadridejosm@gmail.com>
2023-07-27 16:11:09 +0200

    Navbar styled

--- frontend-uknow/src/components/NavbarDesk/NavbarDesk.tsx
+++ frontend-uknow/src/components/NavbarDesk/NavbarDesk.tsx
@@ -1,9 +1,9 @@
-// NavbarDesk.tsx
 import { useEffect, useState } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import styles from './NavbarDesk.module.css'
 import LogoImg from '../../assets/LogoGris.png'
 import LogoText from '../../assets/LogoText.svg'
+import { BellFill, Heart, Person, PlusCircle, Search } from 'react-bootstrap-icons'
 
 export const NavbarDesk = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0)
@@ -24,7 +24,7 @@ export const NavbarDesk = () => {
 
   return (
     <section className={styles.navBarSection}>
-      <div className={styles.navBarItems}>
+      <div className={styles.navBarContainer}>
         <div>
           <Nav.Item className={styles.logoText}>
             <Nav.Link href='/home'>
@@ -39,22 +39,42 @@ export const NavbarDesk = () => {
             </Nav.Link>
           </Nav.Item>
         </div>
-        <div className={styles.navBarLinks}>
-          <Nav.Item as='li'>
-            <Nav.Link className={styles.item} href='/addcourse'>
-              New Course
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link className={styles.item} href='/course'>
-              My courses
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link className={styles.item} href='/loginsignup'>
-              Profile
-            </Nav.Link>
-          </Nav.Item>
+        <div className={styles.navBarItems}>
+          <div className={styles.navBarLinks}>
+            <Nav.Item as='li'>
+              <Nav.Link href='/addcourse'>
+                <span className={styles.item}>
+                  New Course <PlusCircle className={styles.icon} />
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item as='li'>
+              <Nav.Link href='/course'>
+                <span className={styles.item}>
+                  My courses <Heart className={styles.icon} />
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item as='li'>
+              <Nav.Link href='/loginsignup'>
+                <span className={styles.item}>
+                  Profile <Person className={styles.icon} />
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+          </div>
+          <div className={styles.iconsContainer}>
+            <Nav.Item as='li'>
+              <Nav.Link href='/searchbykeyword'>
+                <Search className={styles.iconBubble} />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item as='li'>
+              <Nav.Link href='/notifications'>
+                <BellFill className={styles.iconBubble} />
+              </Nav.Link>
+            </Nav.Item>
+          </div>
         </div>
       </div>
     </section>
