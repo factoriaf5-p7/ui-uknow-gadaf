be52ad406460ed67559bd9ad2683a0de8f5824ae
andreubltzr <andreubltzr11@gmail.com>
2023-07-28 16:13:00 +0200

    changed routing method to declarative

--- frontend-uknow/src/App.tsx
+++ frontend-uknow/src/App.tsx
@@ -1,5 +1,28 @@
+import { Route, Routes } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage/LandingPage'
+import CourseDetail from './components/CourseDetail/CourseDetail'
+import { Profile } from './components/Profile/Profile'
+import { AddCoursePage } from './pages/AddCoursePage'
+import { AllUsersPage } from './pages/AllUsersPage'
+import { Auth } from './pages/Auth/Auth'
+import { CoursePage } from './pages/CoursePage'
+import { HomePage } from './pages/HomePage/HomePage'
+
 function App () {
-  return <></>
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/course' element={<CoursePage />} />
+        <Route path='/addcourse' element={<AddCoursePage />} />
+        <Route path='/allusers' element={<AllUsersPage />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/course/:id' element={<CourseDetail />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
