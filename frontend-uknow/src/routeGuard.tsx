266d4b26326b4adc655bc2d61027f0d0775ff4da
andreubltzr <andreubltzr11@gmail.com>
2023-07-29 19:18:34 +0200

    user authentication done

--- frontend-uknow/src/routeGuard.tsx
+++ frontend-uknow/src/routeGuard.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 interface PrivateRouteProps {
     element: React.ReactNode;
@@ -7,7 +8,33 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ element }) => {
   const token = sessionStorage.getItem('token')
-  return token !== null ? <>{element}</> : <Navigate to='/auth' />
+  const [isTokenValid, setIsTokenValid] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    if (!token) {
+      setIsTokenValid(false)
+    } else {
+      const verifyToken = async () => {
+        try {
+          const res = await axios.post('http://localhost:3000/api/auth/verify', { token })
+          console.log(res.data)
+          setIsTokenValid(true)
+        } catch (error) {
+          console.log(error)
+          setIsTokenValid(false)
+        }
+      }
+      verifyToken()
+    }
+  }, [token])
+
+  if (isTokenValid === null) {
+    return <Navigate to='/auth' />
+  } else if (isTokenValid === false) {
+    return <Navigate to='/auth' />
+  } else {
+    return <>{element}</>
+  }
 }
 
 export default PrivateRoute
