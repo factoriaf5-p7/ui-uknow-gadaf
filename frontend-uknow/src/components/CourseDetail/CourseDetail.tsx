cbd1ae161eb3f4887ee20803d5fea144ec5fd46b
Georgina <georginatomass@gmail.com>
2023-07-31 13:08:52 +0200

    courseDetail

--- frontend-uknow/src/components/CourseDetail/CourseDetail.tsx
+++ frontend-uknow/src/components/CourseDetail/CourseDetail.tsx
@@ -32,7 +32,7 @@ const CourseDetail = () => {
         <h2>{courseDetails.name}</h2>
         <p>Category: {courseDetails.category}</p>
         <p>Difficulty: {courseDetails.difficulty}</p>
-        <p>Rating: <StarRating stars={courseDetails.rating}/> {courseDetails.rating}</p>
+        <p>Rating: <StarRating stars={courseDetails.rating} /> {courseDetails.rating}</p>
         <p>Price: {courseDetails.price}</p>
         <p>Description: {courseDetails.content}</p>
         <img src={courseDetails.image} alt={courseDetails.name} style={{ width: '250px' }} />
