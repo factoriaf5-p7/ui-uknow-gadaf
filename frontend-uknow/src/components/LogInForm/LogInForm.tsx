dce78e62f998dd6ef021e414a29c42cb1bbe608e
andreubltzr <andreubltzr11@gmail.com>
2023-08-01 18:14:37 +0200

    fixed login

--- frontend-uknow/src/components/LogInForm/LogInForm.tsx
+++ frontend-uknow/src/components/LogInForm/LogInForm.tsx
@@ -6,10 +6,6 @@ import styles from './LogInForm.module.css'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-// eslint-disable-next-line no-unused-vars
-interface ApiError {
-  message: string;
-}
 export const LogInForm = () => {
   const navigate = useNavigate()
   const initialState = { email: '', password: '' }
@@ -17,25 +13,23 @@ export const LogInForm = () => {
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    try {
-      const response = await axios.post(
-        'http://localhost:3000/api/auth/login',
-        formData,
-        {
-          headers:
-        { 'Content-Type': 'application/JSON' }
-        })
-      sessionStorage.setItem('token', response.data.token)
-      navigate('/home')
-      setFormData(initialState)
-    } catch (error: ApiError | any) {
-      if (error.response && error.response.data && error.response.data.message) {
-        return alert('Invalid username or password')
-        // modify to add red border to each field and get the message into the div of the form
-      } else {
-        console.log('An error occurred while logging in.')
+    console.log('Form submitted!')
+    console.log(formData)
+    const response = await axios.post(
+      'http://localhost:3000/api/auth/login',
+      formData,
+      {
+        headers: { 'Content-Type': 'application/JSON' }
       }
-    }
+    )
+    const token = response.data.payload.token
+    const id = response.data.payload.id
+    console.log(token)
+    console.log(response.data)
+    localStorage.setItem('token', token)
+    localStorage.setItem('id', id)
+    navigate('/home')
+    setFormData(initialState)
   }
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
