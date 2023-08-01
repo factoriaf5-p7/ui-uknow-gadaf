534ab09be1fc306bc7613f5545bb9d5aa4f1b62d
Teresa <teresamadridejosm@gmail.com>
2023-08-01 09:57:39 +0200

    CoursePage css added

--- frontend-uknow/src/pages/CoursePage/CoursePage.tsx
+++ frontend-uknow/src/pages/CoursePage/CoursePage.tsx
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import { StarRating } from '../../components/StarRating/StarRating'
 import DemoImg from '../../assets/demoImg.jpeg'
 import styles from './CoursePage.module.css'
+import { PlayFill, Clock, BarChartFill } from 'react-bootstrap-icons'
 
 const CourseDetail = () => {
   const { id } = useParams()
@@ -41,26 +42,46 @@ const CourseDetail = () => {
         <section>
           <img src={DemoImg} alt={courseDetails.name} className={styles.courseImg} />
           <hr />
-          <div className={styles.ratingLevel}>
-          <StarRating stars={courseDetails.rating} /> {courseDetails.rating}
-          <p>{courseDetails.difficulty}</p>
-          </div>
           <h2>{courseDetails.name}</h2>
-
-          <div className={styles.priceButton}>
-            <p className={styles.priceCourse}>{courseDetails.price}$</p>
-            <div className={styles.buyButton} onClick={handleBuyCourse}>
-              Buy course
+          <div className={styles.ratingDifficulty}>
+            <div className={styles.rating}>
+              <StarRating stars={courseDetails.rating} /> {courseDetails.rating}
             </div>
+            <div className={styles.difficulty}>
+              <BarChartFill />
+              {courseDetails.difficulty}
+            </div>
+
+          </div>
+          <div className={styles.buyButton} onClick={handleBuyCourse}>
+            <p className={styles.priceCourse}>Buy for {courseDetails.price}$</p>
           </div>
           <hr />
 
           <p className={styles.courseDescription}>{courseDetails.courseDescription}</p>
           <p>{courseDetails.category}</p>
         </section>
+
         <section className={styles.courseContent}>
           <h4 className={styles.courseContentTitle}>Course's content</h4>
           <p>{courseDetails.content}</p>
+
+          <a href={courseDetails.videoUrl} className={styles.videoLink}>
+            <div className={styles.videoBox}>
+              <div className={styles.squareButton}>
+                <button className={styles.circleButton}>
+                  <PlayFill className={styles.playIcon} />
+                </button>
+              </div>
+              <div className={styles.videoInfo}>
+                <h3 className={styles.videoTitle}>What is Scala for Spark?</h3>
+                <p className={styles.videoDuration}>
+                  <Clock className={styles.clockIcon} /> {courseDetails.videoDuration} 20 Minutes
+                </p>
+              </div>
+            </div>
+          </a>
+
         </section>
       </Container>
     </>
