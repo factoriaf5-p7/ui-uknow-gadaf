83a9452c2e2c8b6db129fcbc7c113b739d91ab20
Teresa <teresamadridejosm@gmail.com>
2023-08-02 17:02:20 +0200

    styles

--- frontend-uknow/src/components/BackButton/BackButton.tsx
+++ frontend-uknow/src/components/BackButton/BackButton.tsx
@@ -4,8 +4,8 @@ import styles from './BackButton.module.css'
 export const BackButton = () => {
   return (
     <div className={styles.backButton}>
-        <a href='../../home'><ArrowLeft className={styles.backButtonIcon} /></a>
-      
+      <a href='../../home'><ArrowLeft className={styles.backButtonIcon} /></a>
+
     </div>
   )
 }
