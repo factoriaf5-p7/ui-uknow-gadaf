4d3e370693a25fe42f318120048de77fe8be708c
Teresa <teresamadridejosm@gmail.com>
2023-08-02 19:47:33 +0200

    Landing Page styles

--- frontend-uknow/src/pages/LandingPage/LandingPage.tsx
+++ frontend-uknow/src/pages/LandingPage/LandingPage.tsx
@@ -10,24 +10,31 @@ export const LandingPage = () => {
       {/* <div className={styles.interrogantes}>
         <div className={styles.i1}>?</div><div className={styles.i2}>?</div><div className={styles.i3}>?</div>
       </div> */}
-      <div className={styles.container}>
-        <section className={styles.logo}>
-          <img src={Logo} alt='logo' className={styles.logoImg} />
-          <h1 className={styles.textLogoAnimation}>
-            <span className={styles.u}>u</span>
-            <span className={styles.guion}>-</span>
-            <span className={styles.k}>k</span><span className={styles.n}>n</span><span className={styles.o}>o</span><span className={styles.w}>w</span>
-          </h1>
-        </section>
-        <section className='{styles.buttons'>
-          <Link to='/auth'>
-            <ButtonP className={styles.button} text='Get Started' />
-          </Link>
-          <br /><br />
-          <Link to='/home'>
-            <ButtonS text='Explore'/>
-          </Link>
-        </section>
+      <div className={styles.containerLandingPage}>
+        <div className={styles.contentLandingPage}>
+          <section className={styles.landingPagelogo}>
+            <img src={Logo} alt='logo' className={styles.logoImg} />
+            <h1 className={styles.textLogoAnimation}>
+              <span className={styles.u}>u</span>
+              <span className={styles.guion}>-</span>
+              <span className={styles.k}>k</span><span className={styles.n}>n</span><span className={styles.o}>o</span><span className={styles.w}>w</span>
+            </h1>
+          </section>
+
+          <section className={styles.landingPageText}>
+            <h1 className={styles.slogan}>The new sharing & learning platform </h1>
+            <h2 className={styles.catchPhrase}>Sign up and get 1000 free knowlitos to get you started</h2>
+          </section>
+          <div className={styles.landingPageButtons}>
+            <Link to='/auth'>
+              <ButtonP className={styles.getStartedButton} text='Get started' />
+            </Link>
+            <br /><br />
+            <Link to='/home'>
+              <ButtonS className={styles.exploreButton} text='Explore' />
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   )
