dcf1663fcbdd39713d54375feed91fae73f27084
andreubltzr <andreubltzr11@gmail.com>
2023-08-02 14:20:02 +0200

    fixed bought courses

--- frontend-uknow/src/components/CreatedCourses/CreatedCourses.tsx
+++ frontend-uknow/src/components/CreatedCourses/CreatedCourses.tsx
@@ -2,12 +2,11 @@ import { Container } from 'react-bootstrap'
 import { AllCoursesCard } from '../AllCourses/AllCoursesCard'
 import styles from './CreatedCourses.module.css'
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 
 export const CreatedCourses = () => {
   const [course, setCourse] = useState<any[]>([])
   const userId = localStorage.getItem('id')
-  console.log(`Este es el user ${userId}`)
+
   useEffect(() => {
     const fetchCourse = async () => {
       try {
@@ -25,18 +24,19 @@ export const CreatedCourses = () => {
     <Container className={styles.container}>
       <h6>Created Courses</h6>
       <div className='d-flex flex-wrap justify-content-center' style={{ display: 'inline-block', gap: 10 }}>
-        {course.map((course, i) => (
-          <div key={i}>
-            <Link to={`/course/${course._id}`}>
-              <AllCoursesCard
-                image={course.image}
-                rating={course.rating}
-                title={course.name}
-                price={course.price}
-              />
-            </Link>
-          </div>
-        ))}
+        {course.length > 0
+          ? (
+            <div className='d-flex flex-wrap justify-content-center' style={{ display: 'inline-block', gap: 10 }}>
+              {course.map((course, i) => (
+                <div key={i}>
+                  <AllCoursesCard image={course.image} rating={course.rating} title={course.title} price={course.price} />
+                </div>
+              ))}
+            </div>
+            )
+          : (
+            <p>No created courses available.</p>
+            )}
       </div>
     </Container>
   )
