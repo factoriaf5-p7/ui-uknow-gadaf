deb544f5917f152456fb6d9ac33ee2ba65074a13
Teresa <teresamadridejosm@gmail.com>
2023-08-03 15:36:33 +0200

    Profile responsive fixed

--- frontend-uknow/src/components/ProfileContact/ProfileContact.tsx
+++ frontend-uknow/src/components/ProfileContact/ProfileContact.tsx
@@ -4,12 +4,12 @@ import { ButtonP } from '../ButtonP'
 
 export const ProfileContact = () => {
   return (
-    <Container className={styles.container}>
-      <h2 className={styles.contactTitle}>Contact us</h2>
+    <Container className={styles.containerContact}>
+      <h2 className={styles.profileTitles}>Contact us</h2>
       <Form>
         <Form.Group className='mb-3'>
           <Form.Control
-            className={styles.textField}
+            className={styles.textFieldContact}
             type='text'
             placeholder='Enter your name'
             name='name'
@@ -18,15 +18,20 @@ export const ProfileContact = () => {
 
         <Form.Group className='mb-3'>
           <Form.Control
-            className={styles.textField}
+            className={styles.textFieldContact}
             type='email'
             placeholder='Enter your email'
             name='email'
           />
         </Form.Group>
 
-        <Form.Group>
-          <Form.Control className={styles.textFieldMessage} placeholder='Message'/>
+        <Form.Group className='mb-3'>
+          <Form.Control
+            className={styles.textFieldMessage}
+            type='message'
+            placeholder='Write your message'
+            name='message'
+          />
         </Form.Group>
         <br />
         <ButtonP className={styles.buttonSubmit} text='Send' />
